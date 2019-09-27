cd09b293928fc8c6d0939924add2ff5a51293d53
David <Anomolistic@users.noreply.github.com>
2019-09-27 16:05:17 -0500

    .

--- C/time_arraylist.c
+++ C/time_arraylist.c
@@ -4,27 +4,6 @@
 #include <stdio.h>
 #include <math.h>
 
-void printvis(double x) {
-  for(int i = 0; i < x; i++)
-    printf("-");
-}
-void print2vis(double x, double y) {
-  if(y < x)
-    printf("wrong order!\n");
-
-  for(int i = 1; i < y; i++)
-    if(i == x)
-      printf("|");
-    else
-      printf("-");
-  printf("|\n");
-}
-void print1vis(double y) {
-  for(int i = 1; i < y; i++)
-    printf("-");
-  printf("|\n");
-}
-
 void * ptr(int x){
   int * out = alloc(int);
   *out = x;
@@ -33,22 +12,13 @@ void * ptr(int x){
 
 int main() {
 
-  /*double t0 = doubletime();
-  double sltime = 0;
-  for(double sleeptime = .0001;;sleeptime *= 1.1) {
-    double x = doubletime()-t0;
-    print1vis((x-sltime)/log(sltime)*1000);
-    doublesleep(sleeptime);
-    sltime += sleeptime;
-  }*/
-
-  for(int n = 1;; n*=2) {
+  for(int n = 1000;; n*=1.1) {
 
     double t0 = doubletime();
 
     ArrayList * arr = arraylist_new();
 
-    long total = 0;
+    int total = 0;
     for(int i = 0; i < n*2; i++)
       arraylist_append(arr, ptr(i));
     for(int i = 0; i < n; i++)
@@ -59,86 +29,21 @@ int main() {
       total += *(int*)arraylist_get(arr, i);//(1+n)*n/2
     for(int i = 0; i < n; i++)
       total += *(int*)arraylist_pop(arr);//(1+n)*n/2
-         //(5n+1)*n/2
+            //(5n+1)*n/2
+
     double t1 = doubletime();
 
     arraylist_free_with_all_elements(arr);
 
-    long t_should = (n*5+1)*n/2;
+    int t_should = (n*5+1)*n/2;
     if(total != t_should)
-      printf("Oops! %d, %ld, %ld\n", n, total, t_should);
+      printf("Computational Inaccuracy: n=%d, total=%ld, expected=%ld\n", n, total, t_should);
 
     double t = t1-t0;
     double k = n*7;
 
-    printf("n = %d, K = %lf\n", n, 600*1000*1000/k*t);
+    printf("n = %d, K = %lf\n", n, 600*1000*1000/k*t);//  23.0
 
   }
-
-  return 0;
-
-  /*for(double sleeptime = .00001;;sleeptime *= 1.4) {
-
-  double t0 = doubletime();
-
-  
-
-
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-  doublesleep(sleeptime);
-
-  double t1 = doubletime();
-  double x = (t1-t0)/50-sleeptime;
-  printf("st = %lf -> t = %lf", sleeptime, x);
-  printvis((x+.0001)*300*100);
-  }*/
 }
 
