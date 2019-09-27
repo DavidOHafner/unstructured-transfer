8f0304541391b0fa2db487018d3976a0aa10ef4f
Hafner <hafnerda@naur.cs.grinnell.edu>
2019-09-27 17:56:48 -0500

    added heap, compiles, but untested

---
+++ C/heap.h
@@ -0,0 +1,17 @@
+#ifndef HEAP
+#define HEAP
+
+typedef struct Heap Heap;
+typedef struct {
+  double value;
+  void * payload;
+} HeapEntry;
+
+
+Heap * heap_new();
+void heap_free(Heap * h);
+void heap_push(Heap * h, HeapEntry * entry);
+HeapEntry * heap_pop(Heap * h);
+int heap_size(Heap * h);
+
+#endif
